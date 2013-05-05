2d4e68b0f1f52ad7ba6786093fed917a6aa8ff59
masatanish <masatanish@gmail.com>
2013-05-05 19:08:13 +0900

    prepare rspec framework

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'rspec'
+require 'vtapi'
+
+RSpec.configure do |config|
+
+end
---
+++ spec/vtapi_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Vtapi do
+  pending 'not implemented yet.'
+end
